ae89643f147404f14ebb10cb31c9a6072b820541
Ryo_Okahashi <ryo.okahashi@up-sider.com>
2024-10-11 12:59:29 +0900

    feat: support future version

--- src/components/Policy.tsx
+++ src/components/Policy.tsx
@@ -6,6 +6,7 @@ interface GetPolicyProps {
     serviceId: string
     policyKey: string
     baseUrl: string
+    version?: 'latest' | 'next'
 }
 
 interface PolicyProps extends Omit<GetPolicyProps, 'baseUrl'>{
@@ -17,15 +18,24 @@ interface PolicyProps extends Omit<GetPolicyProps, 'baseUrl'>{
  * Fetches a policy document
  * @param serviceId
  * @param policyKey
+ * @param version - 'latest' or 'next'
  * @param baseUrl
  */
 async function fetchPolicy({
     serviceId,
     policyKey,
+    version,
     baseUrl,
 }: GetPolicyProps) {
-    const url = `${baseUrl}/api/policies?sid=${serviceId}&pkey=${policyKey}`
-    const result = await fetch(new URL(url), {
+    const url = new URL(`${baseUrl}/api/policies`)
+    url.searchParams.append('sid', serviceId)
+    url.searchParams.append('pkey', policyKey)
+
+    const useFutureVersions = version === 'next'
+    if (useFutureVersions) {
+        url.searchParams.append('include', 'futureVersions')
+    }
+    const result = await fetch(url, {
         headers: {
             Accept: 'application/json',
         },
@@ -34,11 +44,41 @@ async function fetchPolicy({
         console.error(result.json())
         throw new Error(`Policy Unavailable: ${result.status}`)
     }
-    return (await result.json()) as {
+    
+    const data = await result.json() as {
         key: string
         version: number
         title: string
         content: string
+        futureVersions?: {
+            id: string
+            policyId: string
+            version: number
+            title: string
+            content: string
+            effectiveFrom: string
+        }[]
+    }
+
+    if (useFutureVersions) {
+        if (data.futureVersions && data.futureVersions.length > 0) {
+            const nextVersion = data.futureVersions[0]
+            return {
+                key: nextVersion.policyId,
+                version: nextVersion.version,
+                title: nextVersion.title,
+                content: nextVersion.content
+            }
+        } else {
+            throw new Error(`Policy Unavailable: No future versions`)
+        }
+    } else {
+        return {
+            key: data.key,
+            version: data.version,
+            title: data.title,
+            content: data.content
+        }
     }
 }
 
@@ -47,14 +87,25 @@ async function fetchPolicy({
  * @param props
  * @constructor
  */
-export default function Policy({ serviceId, policyKey, className, _baseUrlOverride }: PolicyProps) {
+export default function Policy({
+    serviceId,
+    policyKey,
+    className,
+    version = 'latest',
+    _baseUrlOverride
+}: PolicyProps) {
     const [text, setText] = useState('')
     const baseUrl = _baseUrlOverride || DEFAULT_BASE_URL
 
     useEffect(() => {
         ;(async () => {
             try {
-                const data = await fetchPolicy({ serviceId, policyKey, baseUrl })
+                const data = await fetchPolicy({
+                    serviceId,
+                    policyKey,
+                    version,
+                    baseUrl
+                })
                 setText(`# ${data.title}\n\n${data.content}`)
             } catch (e) {
                 if ( e instanceof Error) {
