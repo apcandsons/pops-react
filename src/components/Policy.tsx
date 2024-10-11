f993c91247d66ff7f5dcd83ae3a541a152fadb0d
Ryo_Okahashi <ryo.okahashi@up-sider.com>
2024-10-11 12:42:48 +0900

    refactor: cut out fetch function from components

--- src/components/Policy.tsx
+++ src/components/Policy.tsx
@@ -2,11 +2,44 @@ import Markdown from 'markdown-to-jsx'
 import React, { useEffect, useState } from 'react'
 import { DEFAULT_BASE_URL } from '../config'
 
-interface PolicyProps {
+interface GetPolicyProps {
     serviceId: string
     policyKey: string
+    baseUrl: string
+}
+
+interface PolicyProps extends Omit<GetPolicyProps, 'baseUrl'>{
     className?: string
     _baseUrlOverride?: string
+}
+
+/**
+ * Fetches a policy document
+ * @param serviceId
+ * @param policyKey
+ * @param baseUrl
+ */
+async function fetchPolicy({
+    serviceId,
+    policyKey,
+    baseUrl,
+}: GetPolicyProps) {
+    const url = `${baseUrl}/api/policies?sid=${serviceId}&pkey=${policyKey}`
+    const result = await fetch(new URL(url), {
+        headers: {
+            Accept: 'application/json',
+        },
+    })
+    if (result.status !== 200) {
+        console.error(result.json())
+        throw new Error(`Policy Unavailable: ${result.status}`)
+    }
+    return (await result.json()) as {
+        key: string
+        version: number
+        title: string
+        content: string
+    }
 }
 
 /**
@@ -19,29 +52,16 @@ export default function Policy({ serviceId, policyKey, className, _baseUrlOverri
     const baseUrl = _baseUrlOverride || DEFAULT_BASE_URL
 
     useEffect(() => {
-        const url = `${baseUrl}/api/policies?sid=${serviceId}&pkey=${policyKey}`
         ;(async () => {
             try {
-                const result = await fetch(new URL(url), {
-                    headers: {
-                        Accept: 'application/json',
-                    },
-                })
-                if (result.status !== 200) {
-                    console.error(result.json())
-                    setText(`Policy Unavailable: ${result.status}`)
-                    return
-                }
-                const data = (await result.json()) as {
-                    key: string
-                    version: number
-                    title: string
-                    content: string
-                }
+                const data = await fetchPolicy({ serviceId, policyKey, baseUrl })
                 setText(`# ${data.title}\n\n${data.content}`)
             } catch (e) {
-                const error = e as Error
-                setText(`Policy Unavailable: ${error.message}`)
+                if ( e instanceof Error) {
+                    setText(e.message)
+                }else {
+                    setText('Policy Unavailable')
+                }
             }
         })()
     }, [])
